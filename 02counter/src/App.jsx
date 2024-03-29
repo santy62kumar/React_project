f37d518f039990464aee18edecb0d37670033f39
santy62kumar <s.santoshkumar6205@gmail.com>
2024-03-29 19:18:40 +0530

    added setCounter callback

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -11,7 +11,18 @@ function App() {
 
   const addValue = () => {
     if (counter < 20) { // Check if counter is less than 20
-      setCounter(counter => counter + 1);
+
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // differnce b/w these two statement is that above one if called multiple time it will update only one time because 
+      //It doesn't guarantee that React will batch multiple updates
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      //It ensures that the state update is based on the previous state (prevCounter), not the current state (counter).
+      //This is an example of using the functional form of the setState function provided by the useState hook.
+      //It guarantees that React will batch multiple setState calls into a single update, preventing potential issues with stale state values.
     }
     //counter = counter + 1
     // or setCounter(counter + 1)
